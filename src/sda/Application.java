ef3f72a6526aa0a0f7465632a71f74ce3cfa9627
Szymon Nowak <nowszy94@gmai.com>
2018-03-15 18:55:42 +0100

    randomGenerator features

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -1,14 +1,16 @@
 package sda;
 
 import sda.calculator.Calculator;
+import sda.calculator.RandomGenerator;
 
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(5, 10));
-        System.out.println(calculator.subtract(5, 10));
-        System.out.println(calculator.multiply(5, 10));
-        System.out.println(calculator.divide(5, 10));
-        System.out.println(calculator.divide(5, 0));
+        RandomGenerator randomGenerator = new RandomGenerator();
+        System.out.println(calculator.sum(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
+        System.out.println(calculator.subtract(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
+        System.out.println(calculator.multiply(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
+        System.out.println(calculator.divide(randomGenerator.getSmallNumber(), randomGenerator.getBigNumber()));
+        System.out.println(calculator.divide(randomGenerator.getSmallNumber(), 0));
     }
 }
