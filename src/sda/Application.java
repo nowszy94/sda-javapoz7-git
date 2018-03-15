8e2ca4cdd4629a3f90b1bf7c0bd1464a6e256691
Szymon Nowak <nowszy94@gmai.com>
2018-03-15 18:19:20 +0100

    application class with main

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -3,6 +3,8 @@ package sda;
 public class Application {
     public static void main(String[] args) {
         int a; //TODO check if it is needed
+
+
         System.out.println("Hello world");
     }
 }
